f4c51fbc03ef759fdc7e99de47d633a6b8f74b92
leandro ataulfo <leandroataulfo@gmal.com>
2018-11-13 16:06:16 -0200

    VAlidacao customizada: email nao repetido na insercao de cliente

--- src/main/java/com/leandro/cursomc/Services/Validation/ClienteInsertValidator.java
+++ src/main/java/com/leandro/cursomc/Services/Validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.leandro.cursomc.Services.Validation.utils.BR;
+import com.leandro.cursomc.domain.Cliente;
 import com.leandro.cursomc.domain.enums.TipoCliente;
 import com.leandro.cursomc.dto.ClienteNewDTO;
+import com.leandro.cursomc.repositories.ClienteRepository;
 import com.leandro.cursomc.resources.exception.FieldMessage;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,6 +36,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
        if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
     	   list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
        }
+       
+       Cliente aux = repo.findByEmail(objDto.getEmail());
+       if(aux != null) {
+    	   list.add(new FieldMessage("email", "Email já existe"));
+    	   
+       }
+       
        
 		// inclua os testes aqui, inserindo erros na lista
 		for (FieldMessage e : list) {
--- src/main/java/com/leandro/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/leandro/cursomc/repositories/ClienteRepository.java
@@ -1,11 +1,17 @@
 package com.leandro.cursomc.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.leandro.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Integer> {
 
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
+	
 }
