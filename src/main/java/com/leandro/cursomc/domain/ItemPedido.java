c0ad7184ac3029d34750aa89a34281c43eead779
leandroataulfo <leandroataulfo@gmail.com>
2019-02-28 13:28:16 -0300

    Subtotal e total do pedido

--- src/main/java/com/leandro/cursomc/domain/ItemPedido.java
+++ src/main/java/com/leandro/cursomc/domain/ItemPedido.java
@@ -33,6 +33,12 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
+	
+	
+	public double getsubTotal() {
+		return (preco-desconto)*quantidade;
+	}
+	
 @JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
